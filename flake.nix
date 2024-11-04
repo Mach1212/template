fd4cb9e99f433d64bd6d125f97804d22c4373757
Mach1212 <maciej.pruchnik@gmail.com>
2024-11-04 08:17:59 -0500

    [Feat] Initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Dioxus nightly devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              tailwindcss
+              (
+                rust-bin.selectLatestNightlyWith (toolchain:
+                  toolchain.default.override {
+                    extensions = [
+                      # "rust-src"
+                      "rust-std"
+                      "rustc-codegen-cranelift-preview"
+                      "rust-analyzer-preview"
+                      "rustfmt-preview"
+                    ];
+                    targets = ["wasm32-unknown-unknown"];
+                  })
+              )
+            ];
+          };
+      }
+    );
+}
